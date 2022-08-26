edf8cede42cfaaa1270ec12f18dc1514ee9cc37b
Maira Barriga <104040585+mairabarriga@users.noreply.github.com>
2022-08-25 20:36:51 -0500

    Add files via upload
    COMMIT CLASES EMPRESA, EMPLEADO

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Empleado empleado = new Empleado("Ricardo","ricardo@udea.com","UdeA","administrativo");
+        System.out.println(empleado.getCorreo());
+
+        Empresa empresa = new Empresa("UDEA","calle 2","3245678907","90876261");
+        System.out.println(empresa.getNIT());
+
+    }
+}
