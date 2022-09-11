a5d5965177d7187d56239d7352a355bdf3a23393
juancsherrera <jucasanchez@hotmail.com>
2022-09-11 13:43:49 -0400

    Intento controlador Mov Dinero

--- src/main/java/com/nomina/udeadvance/controlador/ControladorMovDinero.java
+++ src/main/java/com/nomina/udeadvance/controlador/ControladorMovDinero.java
@@ -2,13 +2,84 @@ package com.nomina.udeadvance.controlador;
 
 /*
 import com.nomina.udeadvance.entidades.MovimientoDinero;
-import com.nomina.udeadvance.servicios.ServicioMovimientoDinero;*/
+import com.nomina.udeadvance.servicios.ServicioMovimientoDinero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Entity
+@Table(name="Movimiento_Dinero")
 
+*/
 public class ControladorMovDinero {
 
-    /*@Autowired
-    public ServicioImpMovimientoDinero sie;*/
+
+    /*
+
+        @Autowired
+        private long codigo;
+        @Column(name="monto",nullable = false)
+        private double monto;
+        @Column(nullable = false,length = 30)
+        private String concepto;
+        @ManyToOne
+        @JoinColumn(name = "usuario_nombre")
+        private Empleado usuario;
+        @ManyToOne
+        @JoinColumn(name = "empresa_nombre")
+        private Empresa empresa;
+
+        public Empresa getEmpresa() {
+            return empresa;
+        }
+
+        public void setEmpresa(Empresa empresa) {
+            this.empresa = empresa;
+        }
+
+        public MovimientoDinero(long codigo, double monto, String concepto, Empleado usuario, Empresa empresa) {
+            this.codigo = codigo;
+            this.monto = monto;
+            this.concepto = concepto;
+            this.usuario = usuario;
+            this.empresa = empresa;
+        }
+
+        public MovimientoDinero() {
+        }
+
+        public long getCodigo() {
+            return codigo;
+        }
+
+        public void setCodigo(long codigo) {
+            this.codigo = codigo;
+        }
+
+        public double getMonto() {
+            return monto;
+        }
+
+        public void setMonto(double monto) {
+            this.monto = monto;
+        }
+
+        public String getConcepto() {
+            return concepto;
+        }
+
+        public void setConcepto(String concepto) {
+            this.concepto = concepto;
+        }
+
+        public Empleado getUsuario() {
+            return usuario;
+        }
+
+        public void setUsuario(Empleado usuario) {
+            this.usuario = usuario;
+        }
+    }
+
+*/
 }
+
